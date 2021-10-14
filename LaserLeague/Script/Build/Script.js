577c2d1bd0555ed6261a61cf4a272fdf2a2ea829
philippjenny <42078113+philippjenny@users.noreply.github.com>
2021-10-14 15:53:18 +0200

    Adding Rotate functionality to Lasers

--- LaserLeague/Script/Build/Script.js
+++ LaserLeague/Script/Build/Script.js
@@ -21,7 +21,7 @@ var Script;
         hndEvent = (_event) => {
             switch (_event.type) {
                 case "componentAdd" /* COMPONENT_ADD */:
-                    ƒ.Debug.log(this.message, this.getContainer());
+                    // ƒ.Debug.log(this.message, this.getContainer());
                     break;
                 case "componentRemove" /* COMPONENT_REMOVE */:
                     this.removeEventListener("componentAdd" /* COMPONENT_ADD */, this.hndEvent);
@@ -38,13 +38,18 @@ var Script;
     ƒ.Debug.info("Main Program Template running!");
     let viewport;
     document.addEventListener("interactiveViewportStarted", start);
+    let transform;
     function start(_event) {
         viewport = _event.detail;
-        ƒ.Loop.addEventListener("loopFrame" /* LOOP_FRAME */, update);
-        // ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+        ƒ.Loop.addEventListener("loopFrame" /* LOOP_FRAME */, update); //gameloop, which refreshes the image (When loop is calles, run the Function update)
+        ƒ.Loop.start(); // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+        let graph = viewport.getBranch();
+        let laser = graph.getChildrenByName("Lasers")[0].getChildrenByName("Lasers")[0];
+        transform = laser.getComponent(ƒ.ComponentTransform).mtxLocal; //Den Laser nehmen und ihn um 90° um seine Z-Achse drehen
     }
     function update(_event) {
         // ƒ.Physics.world.simulate();  // if physics is included and used
+        transform.rotateZ(5);
         viewport.draw();
         ƒ.AudioManager.default.update();
     }
