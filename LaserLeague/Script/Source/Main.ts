69bf6fa8296723061b3f608f172a91d64fdb3f88
philippjenny <42078113+philippjenny@users.noreply.github.com>
2021-10-27 20:48:57 +0200

    Collision detection advancements arm_1

--- LaserLeague/Script/Source/Main.ts
+++ LaserLeague/Script/Source/Main.ts
@@ -21,7 +21,8 @@ namespace Script {
     ƒ.Loop.start(ƒ.LOOP_MODE.TIME_REAL, 60);  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
     
     viewport.camera.mtxPivot.translateZ(-30);
-    
+
+
     let graph: ƒ.Node = viewport.getBranch();
     let laser: ƒ.Node = graph.getChildrenByName("lasers")[0].getChildrenByName("all_lasers")[0].getChildrenByName("laser")[0];
     transform = laser.getComponent(ƒ.ComponentTransform).mtxLocal;
@@ -62,11 +63,25 @@ namespace Script {
     ƒ.AudioManager.default.update();
   }
 
+//Collision
   function checkCollision(): void {
-    let laser: ƒ.Node = viewport.getBranch().getChildrenByName("lasers")[0].getChildrenByName("all_lasers")[0].getChildrenByName("laser")[0]
+    let laser: ƒ.Node = viewport.getBranch().getChildrenByName("lasers")[0].getChildrenByName("all_lasers")[0].getChildrenByName("laser")[0].getChildrenByName("arms")[0];
     let beam: ƒ.Node = laser.getChildren()[0];
-    let posLocal: ƒ.Vector3 = ƒ.Vector3.TRANSFORMATION(agent.mtxWorld.translation, beam.mtxWorldInverse, true);
-    console.log(posLocal.toString());
+
+  
+
+    let distance: ƒ.Vector3 = ƒ.Vector3.TRANSFORMATION(agent.mtxWorld.translation, beam.mtxWorldInverse, true);
+
+    let x = beam.getComponent(ƒ.ComponentMesh).mtxPivot.scaling.x/2 + agent.radius;
+    let y = beam.getComponent(ƒ.ComponentMesh).mtxPivot.scaling.y + agent.radius;
+
+      if(distance.x <= (x) && distance.x >= -(x) && distance.y <= y && distance.y >= 0) {
+        console.log('collision!');
+      }
+   
+
+
+    
   }
 
 }
